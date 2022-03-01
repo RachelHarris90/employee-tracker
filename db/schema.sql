26b51cade8dd6334b07b10f96f3ac601c8474bfe
Rachel Harris <rachel.harris@msn.com>
2022-03-01 23:34:42 +1100

    Add primary key each table

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT
@@ -17,7 +17,7 @@ CREATE TABLE roles (
 
 
 CREATE TABLE employees (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     FOREIGN KEY (role_id)
--- server.js
+++ server.js
@@ -6,18 +6,18 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'employee_db'
-  },
-  console.log(`Connected to database.`)
-);
+// const db = mysql.createConnection(
+//   {
+//     host: 'localhost',
+//     user: 'root',
+//     password: process.env.DB_PASSWORD || '',
+//     database: 'employee_db'
+//   },
+//   console.log(`Connected to database.`)
+// );
 
 inquirer
   .prompt([
@@ -28,6 +28,6 @@ inquirer
       choices: ['View all employees', 'Add employee', 'Update employee role', 'View all roles', 'Add role', 'View all departments', 'Add department', 'Quit'],
     },
   ])
-  .then((response) =>
-    response.confirm === response.password
-  );
+  .then((answers =>
+    console.info('Answer:', answers.tasks)
+  ));
