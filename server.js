9955c1f662b38296eccdc2468699f2b1067712e5
Rachel Harris <rachel.harris@msn.com>
2022-03-01 23:04:08 +1100

    Update packages

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+  },
+  console.log(`Connected to database.`)
+);
+
 inquirer
   .prompt([
     {
