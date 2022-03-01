73214e5ee5c843b8dde091f41eb4d92c80d200b7
Rachel Harris <rachel.harris@msn.com>
2022-03-01 23:49:42 +1100

    Change foreign key structure for role_id

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT
@@ -17,11 +17,8 @@ CREATE TABLE roles (
 
 
 CREATE TABLE employees (
-    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(role_id),
-    FOREIGN KEY (employee_id)
-    REFERENCES employees(employee_id)
+    FOREIGN KEY (id) REFERENCES roles(id)
 );
