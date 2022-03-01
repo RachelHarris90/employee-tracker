ee644b2aafed54684c116db4a7bc4790dbb3ae2c
Rachel Harris <rachel.harris@msn.com>
2022-03-01 23:41:29 +1100

    Update foreign key references

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT
@@ -17,11 +17,11 @@ CREATE TABLE roles (
 
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id),
-    FOREIGN KEY (employee)
-    REFERENCES employees(id)
+    REFERENCES roles(role_id),
+    FOREIGN KEY (employee_id)
+    REFERENCES employees(employee_id)
 );
