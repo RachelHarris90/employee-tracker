b2c176f3d7fa5d951d2e2810f8d6d6b364a7af71
Rachel Harris <rachel.harris@msn.com>
2022-03-01 21:35:38 +1100

    Setup DB and produce seed file

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
